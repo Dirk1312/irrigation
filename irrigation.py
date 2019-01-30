8bb646a6834deadef40b593d8c32def63a9927bb
Dirk1312 <39053420+Dirk1312@users.noreply.github.com>
2019-01-30 17:46:28 +0100

    Initial commit of script
    This script gives the possibility to control garden irrigation valves via MQTT commands. In case you want to re-use this script for your own garden irrigation, you must adapt the following settings:
    
    # The pins your valves are connected to the rasperrby pi, e.g.
    PINS     = [2, 3, 4, 9, 10]
    # MQTT Broker ip
    BROKER_ADDRESS = '10.50.50.20'
    # The MQTT topic you publish commands for control the valves
    MQTT_COMMAND_TOPIC     = 'garden/irrigation'
    # The MQTT topic pattern the raspberry pi, e.g. publishes the state of the valves after they were switched to "ON" or "OFF"
    MQTT_VALVE_STATE_TOPIC = 'garden/irrigation/valve-{}-state'
    
    The json command sent via your broker to control the valves must look like this:
    {'valve': 1, 'pin': 4, 'state': 'ON'}
    Parameters / Attributes:
    - valve: The valve "name" in my internal usage. I numbered all my valves from 1 - 5 and so I'm also able to identify them without always having to map them against the pins. This numeric value is also used to replace the placeholder in the MQTT_VALVE_STATE_TOPIC
    - pin: The pin the valve is controlled / connected to raspberry pi, e.g.
    - state: "ON"|"OFF" - the state the valves shall be switched to.

---
+++ irrigation.py
@@ -0,0 +1,92 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#--------------------------------------------------------------------------------
+# irrigation.py für den automatischen Start eingetragen in: 
+# Schauen, ob Script im Hintergrund läuft: ps aux | grep /home/raspberry/irrigation.py
+# 
+#
+# CHANGELOG:
+# 2018-12-15:
+# MQTT-Anbindung für die Ventilsteuerung realisiert und initial getestet
+#
+# 2018-12-24:
+# Anbindung an OpenHAB 2 und Benennung der Schaltungen entsprechend der OpenHAB-
+# IDs
+# Angepasst, dass ab sofort JSON in der Form {'valve': 1, 'pin': 4, 'state': 'ON'}
+# gesendet wird. Der state kann 'ON' oder 'OFF' sein
+#
+# 2019-01-08:
+# Status-Topic eingeführt. Aufbau: garden/irrigation/valve-%d-state
+# Gibt bei geöffnetem Ventil ON und bei geschlossenem Ventil OFF zurück. Hierfür wird
+# nach dem setzen eines GPIO Pins auf HIGH oder LOW geprüft, ob dieser wirklich aktuell
+# den Zustand hat und dann entsprechend eine Status-Meldung verschickt.
+# In /etc/rc.local für den Autostart eingetragen
+# python /home/pi/irrigation.py & > /var/log/irrigation.log 2>&1
+#
+# 2019-01-25:
+# Broker-Url auf 'house.lan' geändert
+#--------------------------------------------------------------------------------
+
+import RPi.GPIO as gpio
+import paho.mqtt.client as mqtt
+import json
+from functools import partial
+
+PINS     = [2, 3, 4, 9, 10]
+
+# MQTT Broker (running on the OpenHAB Server)
+BROKER_ADDRESS = '10.50.50.20'
+MQTT_COMMAND_TOPIC     = 'garden/irrigation'
+MQTT_VALVE_STATE_TOPIC = 'garden/irrigation/valve-{}-state'
+
+def open(pin):
+    gpio.output(pin, gpio.LOW)
+
+
+def close(pin):
+    gpio.output(pin, gpio.HIGH)
+
+def onMessage(client, userdata, message):
+    msg     = str(message.payload.decode("utf-8"))
+    command = json.loads(msg)
+    print(command)
+    if 'ON' == command['state']:
+        open(command['pin'])
+        publishValveState(client, command['valve'], 'ON')
+    elif 'OFF' == command['state']:
+        close(command['pin'])
+        publishValveState(client, command['valve'], 'OFF')
+ 
+def onConnect(client, userdata, flags, rc):
+    client.subscribe(MQTT_COMMAND_TOPIC, 0)
+    
+def publishValveState(client, valve, state):
+    mqttValveStateTopic = createMqttValveStateTopic(valve)
+    client.publish(mqttValveStateTopic, state)
+    
+def createMqttValveStateTopic(valve):
+    return MQTT_VALVE_STATE_TOPIC.format(valve)
+
+def main(prompt):
+    try:
+        gpio.setmode(gpio.BCM)
+        gpio.setup(PINS, gpio.OUT, initial=gpio.HIGH) 
+
+        client = mqtt.Client('irrigation')
+        client.on_connect    = onConnect
+        client.on_message    = onMessage
+        client.connect(BROKER_ADDRESS) 
+        client.loop_forever()   
+        
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.loop_stop()
+        client.unsubscribe(MQTT_COMMAND_TOPIC)
+        client.disconnect()
+        gpio.cleanup()  # this ensures a clean exit
+        print('QUIT')
+
+if __name__ == '__main__':
+    main('Action? ')
